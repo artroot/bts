ade86691cf7ced84be884153699deb0cb41c342a
artroot <art90com@gmail.com>
2018-02-25 23:42:09 +0200

    Add scss/css for garaphic builder; Add bootstrap JS

--- views/version/graph.php
+++ views/version/graph.php
@@ -8,15 +8,15 @@
 	use app\components\SVG;
 
 ?>
-<div style="width: 100%; height: 400px;">
-<div style="padding-top: 20px; width: 2%; display: inline-block; height: 97%;">
-	<div style="position: relative; height: 100%;">
+<div class="graphic">
+<div class="scaleY">
+	<div>
 		<?php for($i = $scales[SVG::SVG_Y]['max']; $i >= 0; $i -= $scales[SVG::SVG_Y]['step']): ?>
-			<div style="position: absolute; width: 100%; text-align: right; bottom: <?= $i*100/$scales[SVG::SVG_Y]['max'] ?>%;"><?= $i ?></div>
+			<div style="bottom: <?= $i*100/$scales[SVG::SVG_Y]['max'] ?>%;"><?= $i ?></div>
 		<?php endfor; ?>
 	</div>
 </div>
-<svg width="97%" height="97%" style="padding: 10px; display: inline-block;" xmlns="http://www.w3.org/2000/svg">
+<svg width="97%" height="97%" xmlns="http://www.w3.org/2000/svg">
 	<g>
 		<line stroke-linecap="undefined" stroke-linejoin="undefined" id="svg_g_g"
 			  y2="0%"
@@ -67,10 +67,10 @@
 	<?php endforeach; ?>
 </svg>
 
-<div style="padding-left: 10px; padding-right: 10px; margin-left: 2%; width: 98%; display: block; height: 2%;">
-	<div style="position: relative; width: 100%;">
+<div class="scaleX">
+	<div>
 		<?php for($i = 0; $i <= count($scales[SVG::SVG_X]['coords']); $i++): ?>
-			<div style="position: absolute; left: <?= $i*100/count($scales[SVG::SVG_X]['coords']) ?>%;"><?= $i ?></div>
+			<div style="left: <?= $i*100/count($scales[SVG::SVG_X]['coords']) ?>%;"><?= $i ?></div>
 		<?php endfor; ?>
 	</div>
 </div>
