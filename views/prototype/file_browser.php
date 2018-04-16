ce4802288d905b48e3423b53a67c3982e1ac6223
artroot <art90com@gmail.com>
2018-04-16 13:32:58 +0300

    Refactored filebrowser root folder name like prototype index

--- views/prototype/file_browser.php
+++ views/prototype/file_browser.php
@@ -21,25 +21,24 @@ $urlPath = [];
         $urlPath[] = $folder;
         ?>
         <li class="<?= count($model->getTree())-1 == $key ? 'active' : '' ?>">
-            <?= count($model->getTree())-1 == $key ? $folder : sprintf('<a href="%s" data-pjax="prototypeMainForm">%s</a>',
-                Url::to(['prototype/filebrowser', 'id' => $model->id, 'back_to' => implode('/', $urlPath)]), $folder) ?>
+            <?= count($model->getTree())-1 == $key ? ($key == 0 ? $model->index() : $folder) : sprintf('<a href="%s" data-pjax="prototypeMainForm">%s</a>',
+                Url::to(['prototype/filebrowser', 'id' => $model->id, 'back_to' => implode('/', $urlPath)]), $key == 0 ? $model->index() : $folder) ?>
         </li>
     <?php endforeach; ?>
 </ol>
-<div class="file-browser thumbnail" style="max-height: 300px; overflow: auto;">
+<div class="file-browser thumbnail" style="max-height: 300px; background-color: #f5f5f5; overflow: auto;">
     <?= $form->field($model, 'index_file_name')->radioList($model->getPrototypeFiles(), [
         'item' => function($index, $label, $name, $checked, $value) use ($model) {
             $return = '<p><label>';
-            if(!is_dir(Yii::$app->basePath . '/web' . $model->path . '/' .  $label)) {
-                $valuePath = $model->getTree();
+            $valuePath = $model->getTree();
+            if(!is_dir(Yii::$app->basePath . '/web/prototypes/' . implode('/', $valuePath) . '/' .  $label)) {
                 array_shift($valuePath);
                 array_push($valuePath, $label);
                 $return .= '<span class="glyphicon glyphicon-file"></span> <input type="radio" ' . ($model->index_file_name == implode('/', $valuePath) ? 'checked' : '') . ' name="' . $name . '" value="' .  implode('/', $valuePath) . '" tabindex="3"> <span>' . $label   . '</span>';
             }else{
-                $urlPath = $model->getTree();
-                array_push($urlPath, $label);
-                $return .= '<span class="glyphicon glyphicon-folder-open"></span> 
-<a href="' . Url::to(['prototype/filebrowser', 'id' => $model->id, 'browse_to' => implode('/', $urlPath)]) . '" data-pjax="prototypeMainForm">' . $label . '</a>';
+                array_push($valuePath, $label);
+                $return .= '<a href="' . Url::to(['prototype/filebrowser', 'id' => $model->id, 'browse_to' => implode('/', $valuePath)]) .
+                    '" data-pjax="prototypeMainForm"><span class="glyphicon glyphicon-folder-open"></span> &nbsp; <span>' . $label . '</span></a>';
             }
             $return .= '</label></p>';
 
